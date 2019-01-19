04e68f26f1487fd909ff0a852b14709c42adbff8
pikulet <jycyeo@yahoo.com.sg>
2019-01-19 18:19:57 +0800

    Add function calls on click of buttons

---
+++ js/sidebar.js
@@ -0,0 +1,15 @@
+function play() {
+  $("#test").text('play');
+}
+
+function stop() {
+  $("#test").text('stop');
+}
+
+function refresh() {
+  $("#test").text('refresh');
+}
+
+function exort() {
+  $("#test").text('exort');
+}
