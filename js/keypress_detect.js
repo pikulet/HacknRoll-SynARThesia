41e611b5dae42e5339b27b0e786fa39cdf0dda79
pikulet <jycyeo@yahoo.com.sg>
2019-01-19 20:47:17 +0800

    Restore play-pause functionality

--- js/keypress_detect.js
+++ js/keypress_detect.js
@@ -1,13 +1,13 @@
 function generateNoteMap() {
   var letters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i',
-  	'a', 's', 'd', 'f', 'h', 'j', 'k', 'l',
-  	'z', 'x', 'c', 'v', 'b', 'n', 'm', ','
+    'a', 's', 'd', 'f', 'h', 'j', 'k', 'l',
+    'z', 'x', 'c', 'v', 'b', 'n', 'm', ','
   ];
   var notes = [
-  	// "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
-  	"C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5",
-  	"C5", "D5", "E5", "F5", "G5", "A5", "B5", "C6",
-  	"C6", "D6", "E6", "F6", "G6", "A6", "B6", "C7",
+    // "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
+    "C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5",
+    "C5", "D5", "E5", "F5", "G5", "A5", "B5", "C6",
+    "C6", "D6", "E6", "F6", "G6", "A6", "B6", "C7",
   ];
 
   var note_map = {}
@@ -26,12 +26,12 @@ var recording = "";
 
 
 function genRGB() {
-	var rgb = new Array(3);
-	for (var i = 0; i < 3; i++) {
-		rgb[i] = Math.floor(Math.random() * 255);
-	}
+  var rgb = new Array(3);
+  for (var i = 0; i < 3; i++) {
+    rgb[i] = Math.floor(Math.random() * 255);
+  }
 
-	return rgb;
+  return rgb;
 }
 
 function synart(note, recording) {
@@ -54,19 +54,21 @@ function synart(note, recording) {
 
 
 $(document).keypress(function(event) {
-  var note = event.key
-  var song = document.getElementById('song').textContent;
-  song += " " + note;
-  $("#song").text(song);
+  if (playing) {
+    var note = event.key
+    var song = document.getElementById('song').textContent;
+    song += " " + note;
+    $("#song").text(song);
 
-  if (note == '1') {
-  	//start timer
-  	startTime = Date.now();
-  	recording = "";
-  } else if (note == '2') {
-  	saveTextAsFile(recording);
-  } else if (note in note_map) {
-	recording = synart(note, recording);
+    if (note == '1') {
+      //start timer
+      startTime = Date.now();
+      recording = "";
+    } else if (note == '2') {
+      saveTextAsFile(recording);
+    } else if (note in note_map) {
+      recording = synart(note, recording);
+    }
   }
 });
 
@@ -75,5 +77,3 @@ $(document).keypress(function(event) {
 function exportRecording() {
   saveTextAsFile(recording);
 }
-
-
