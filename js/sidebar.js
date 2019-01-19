bf64f22b535a9329cc9c4117dff33aba12791ca5
pikulet <jycyeo@yahoo.com.sg>
2019-01-19 20:35:53 +0800

    Add play-pause button effect

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,20 @@
-function play() {
-  $("#test").text('play');
+var playing = true;
+const play_source = "img/play-button.png";
+const pause_source = "img/round-pause-button.png";
+
+function playPause() {
+  var img = document.getElementById('play-pause');
+  if (playing) {
+    img.src = pause_source;
+    playing = false;
+  } else {
+    img.src = play_source;
+    playing = true;
+  }
 }
 
-function stop() {
-  $("#test").text('stop');
+function settings() {
+  $("#test").text('settings');
 }
 
 function refresh() {
