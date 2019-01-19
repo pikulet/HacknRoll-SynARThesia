7f5c5d9a68b1f507a76732aebf22976f9d90c97f
pikulet <jycyeo@yahoo.com.sg>
2019-01-19 13:50:13 +0800

    Add button detection and display

--- js/keypress_detect.js
+++ js/keypress_detect.js
@@ -1,3 +1,26 @@
-$(document).keypress(function() {
-  $("h1#title").text('gotcha');
+function generateNoteMap() {
+  var letters = ['a', 's', 'd', 'f', 'h', 'j', 'k', 'l'];
+  var num = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  var note_map = {}
+  letters.map(function(letter, index) {
+    note_map[letter] = num[index];
+  });
+
+  return note_map;
+}
+
+var note_map = generateNoteMap;
+
+function synart(note) {
+  note_map[note]
+}
+
+$(document).keypress(function(event) {
+  var note = event.key
+  var song = document.getElementById('song').textContent;
+  song += " " + note;
+  $("#song").text(song);
+
+  synart(note);
 });
